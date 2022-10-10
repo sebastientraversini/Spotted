d95d47ca9bc43640eb870aa20126bae27e0f2068
Florianquadri <florianquadri@gmail.com>
2022-10-10 16:10:41 +0200

    req.params.id pour test

--- routes/users.js
+++ routes/users.js
@@ -5,4 +5,15 @@ router.get("/", function (req, res, next) {
   res.send("Got a response from the users route");
 });
 
+router.get("/:id", function (req, res, next) {
+  if(req.params.id !== "1234") {
+    throw new Error("User does not exist")
+    return
+  }
+  res.send({
+    "id" : req.params.id,
+    "nom": "ta mère"
+  });
+});
+
 export default router;
