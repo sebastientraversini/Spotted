907846827f7277e6584c7e1b2ada3a6aebec85a5
sebastientraversini <72557059+sebastientraversini@users.noreply.github.com>
2022-10-31 16:08:23 +0100

    petit push vérificatif
    push debut notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,4 +1,6 @@
 import express from "express";
+import Note from '../models/notes.js';
+
 const router = express.Router();
 
 router.get("/", function (req, res, next) {
@@ -7,5 +9,16 @@ router.get("/", function (req, res, next) {
 router.get("/hello", function (req, res, next) {
     res.send("hello");
   });
+
+  router.post('/insert',function (req, res, next){
+    let item = {
+      title:req.body.title,
+      content:req.body.content,
+      author:req.body.author
+    }
+    let data = new UserData(item);
+    data.save();
+    res.redirect('/')
+  })
 
 export default router;
