a35376fcc9424786e15de7a824a06016d8631a46
sebastientraversini <72557059+sebastientraversini@users.noreply.github.com>
2022-11-10 09:59:30 +0100

    note model

--- models/note.js
+++ models/note.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import users from './user.js';
+import places from './place.js';
 
 const Schema = mongoose.Schema;
 // Define the schema for notes
@@ -7,8 +8,34 @@ const noteSchema = new Schema({
     userId: {
         type: Schema.Types.ObjectId,
         ref: 'User'
+      },
+      placeId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Place'
+      },
+      note: {
+        type: Number,
+          required: true,
+          validate: {
+            validator: validateANote,
+            message: '{VALUE} is not a valid note'
+          }
+        },
+      text: {
+        type: String
       }
     });
+
+// Validate a note
+function validateANote(value) {
+  return Number.isNumber(value) && isBetween1And5(value);
+}
+
+function isBetween1And5(value) {
+  return value >= 1 && value <= 5;
+}
+
+
 // Create the model from the schema and export it
 
 
--- routes/notes.js
+++ routes/notes.js
@@ -1,24 +1,32 @@
 import express from "express";
-import Note from '../models/notes.js';
+import Note from '../models/note.js';
 
 const router = express.Router();
 
 router.get("/", function (req, res, next) {
-  res.send("Got a response from the notes route and it works well");
+  res.send("wallah ça fonctionne");
 });
-router.get("/hello", function (req, res, next) {
+/* router.get("/hello", function (req, res, next) {
     res.send("hello");
-  });
+  }); */
 
   router.post('/insert',function (req, res, next){
     let item = {
-      title:req.body.title,
-      content:req.body.content,
-      author:req.body.author
+      note:req.body.note,
+      text:req.body.text
     }
     let data = new UserData(item);
     data.save();
     res.redirect('/')
   })
+
+
+ 
+
+  
+
+
+
+  
 
 export default router;
