cb9280569ba40416ce984c62ecf684f165f4db19
Neckroo <neib.ouakel@gmail.com>
2022-11-14 10:42:40 +0100

    updates doc
    updates

--- routes/places.js
+++ routes/places.js
@@ -4,6 +4,8 @@ import Place from '../models/place.js';
 
 const router = express.Router();
 
+
+
 router.get("/", function (req, res, next) {
   res.send("Got a response from the Places route");
   /* Place.find().then(function (doc) {
@@ -11,6 +13,8 @@ router.get("/", function (req, res, next) {
 
   }) */
 });
+
+
 
 router.post('/',function (req, res, next){
   let item = {
@@ -72,3 +76,93 @@ res.redirect('/')
 })
 
 export default router;
+
+
+
+/**
+ * @api {get} /places/:id Request a place's information
+ *  
+ * @apiName GetPlace
+ * @apiGroup Place
+ *
+ * @apiParam {Number} id Place id 
+ * @apiParam {String} name Place name 
+ *
+ * @apiSuccess {String} name Place name
+ * @apiSuccess {String} canton  Place canton
+ * @apiSuccess {Objects[]} location  Place location
+ * @apiSuccess {Strings[]} pictures Place pictures
+ * @apiSuccess {Strings[]} notes Place notes
+ * @apiSuccess {String[]} tags Place tags
+ * 
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "name": "chateau de Chillon",
+ *       "canton": "Vaud",
+ *        "location": "{
+ *        1324324234.23,
+ *        234234234234.76556
+ *        }",
+ * "pictures": "{
+ *        1,
+ *        2,
+ *        3
+ *          }",
+ * "notes": "{
+ *        stars : 3,
+ *        text: tréjoli,
+ *          }",
+ * "tags": "{chateau,
+ *           Lac}"
+ *     }
+ */
+
+/**
+ * @api {post} /users/:id add Place
+ *  
+ * @apiName AddPlace
+ * @apiGroup Place
+ * 
+ * @apiParam {String} name Place name, mandatory
+ * @apiParam {String} canton User canton, mandatory
+ * @apiParam {String} location Place location, mandatory
+ * @apiParam {Objects[]} pictures  User pictures, not mandatory
+ * @apiParam {Strings[]} notes  User notes, not mandatory
+ * @apiParam {Strings[]} tags  User tags, not mandatory
+ * 
+ * 
+ * @apiParamExample Example Body:
+ *    {
+ *     "name": "Chateau de Chillon",
+ *    "canton": "Vaud",
+ *   "location": "{
+ *        1324324234.23,
+ *        234234234234.76556
+ *        }",
+ * "pictures": "{
+ *       1,
+ *      2,
+ *    3
+ *   } ,
+ * "notes": "{
+ *       stars : 3,
+ *      text: tréjoli
+ *  }",
+ * "tags": "{chateau,
+ *         Lac}"
+ * }
+ * 
+ * 
+ * 
+ * @apiSuccess {String} firstName User name
+ * @apiSuccess {String} surname  User surname
+ * @apiSuccess {String} password  User password
+ * 
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "votre user à été créé !"
+ *       
+ *     }
+ */
