0af683303a8920d841c21de2b02ed78016a696d7
Florianquadri <florianquadri@gmail.com>
2023-01-16 11:03:50 +0100

    update api place filtree

--- routes/places.js
+++ routes/places.js
@@ -70,7 +70,7 @@ router.get("/", async function (req, res, next) {
     console.log(canton);
     const places = await Place.find({ canton: canton }).exec();
     if (places.length == 0) {
-      return res.send("no places in this canton")
+      return [];
     }
     return res.send(places)
   }
